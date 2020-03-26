4a37f2452d7fd51df97ef7c49fac7501a0f4820b
Sergei Vasilev <vasiliev05@gmail.com>
2020-03-26 16:13:50 +0300

    Change api by REST

--- config/routes/v1.php
+++ config/routes/v1.php
@@ -2,21 +2,33 @@
 
 return [
     [
-        'methods' => ['get'],
+        'methods' => ['post'],
         'name' => 'generate-start-dataset',
         'path' => '/generate-start-dataset',
-        'handler' => App\Action\GenerateStartDatasetAction::class,
+        'handler' => App\Action\Product\GenerateStartDatasetAction::class,
+    ],
+    [
+        'methods' => ['get'],
+        'name' => 'order-list',
+        'path' => '/orders',
+        'handler' => App\Action\Order\ListAction::class,
     ],
     [
         'methods' => ['post'],
-        'name' => 'create-order',
-        'path' => '/order/create',
+        'name' => 'order-create',
+        'path' => '/orders',
         'handler' => App\Action\Order\CreateAction::class,
     ],
     [
         'methods' => ['post'],
-        'name' => 'pay-order',
-        'path' => '/order/pay',
+        'name' => 'order-pay',
+        'path' => '/orders/{id}/pay',
         'handler' => App\Action\Order\PayAction::class,
+    ],
+    [
+        'methods' => ['get'],
+        'name' => 'product-list',
+        'path' => '/products',
+        'handler' => App\Action\Product\ListAction::class,
     ],
 ];
